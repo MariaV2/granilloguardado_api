e01444b3672f095dd559b48f3e14c1fc202cd700
MariaV2 <ia.majogranillo@ufg.edu.sv>
2025-10-16 20:00:36 -0600

    Practica Laravel

---
+++ granilloguardado_20251016/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ZonaController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/zonas',[ZonaController::class,'obtenerZonas']); //PLURAL
+Route::get('/zonas',[ZonaController::class,'obtenerZona']); //SINGULAR
