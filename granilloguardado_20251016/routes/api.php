fbf6294c452a91373d7b63448aa8c167416e6c23
MariaV2 <ia.majogranillo@ufg.edu.sv>
2025-10-26 23:16:24 -0600

    Tarea completada y funcionando wiii

--- granilloguardado_20251016/routes/api.php
+++ granilloguardado_20251016/routes/api.php
@@ -3,11 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZonaController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/nuevousuario', [AuthController::class, 'crearUsuario']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/usuario', [AuthController::class, 'obtenerUsuario']);
+});
 
 Route::get('/zonas',[ZonaController::class,'obtenerZonas']); //PLURAL
 Route::get('/zona/{idzona}',[ZonaController::class,'obtenerZona']); //SINGULAR
